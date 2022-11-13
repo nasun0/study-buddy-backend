38984443949111d18a748d0ed5ae482758e13823
nasuno <nasuno@nasunos-Laptop.local>
2022-11-13 10:00:56 -0600

    final commit

--- app.js
+++ app.js
@@ -6,17 +6,45 @@ app.use(cors());
 
 const port = process.env.port || 3030;
 
-let rooms = {
-    ecsw: {
-        1.123: "false",
-        1.124: "false",
-        1.125: "false",
-        1.126: "false",
+let rooms = [
+    {
+        buildings: "ECSW - Engineering And Computer Science West",
+        floorRoom: "1.123",
+        available: "true",
     },
-};
+    {
+        buildings: "ECSW - Engineering And Computer Science West",
+        floorRoom: "1.124",
+        available: "true",
+    },
+    {
+        buildings: "ECSW - Engineering And Computer Science West",
+        floorRoom: "1.125",
+        available: "true",
+    },
+    {
+        buildings: "ECSW - Engineering And Computer Science West",
+        floorRoom: "1.126",
+        available: "true",
+    },
+];
+
 app.get("/room", (req, res) => {
     console.log(req.query.occupied);
-    rooms[req.query.building][req.query.room] = req.query.occupied;
+    let room = req.query.room;
+    for (let i = 0; i < rooms.length; i++) {
+        let curRoom = rooms[i];
+
+        if (curRoom.floorRoom == room) {
+            let string = req.query.occupied;
+            let available = "";
+
+            if (req.query.occupied == "true") available = "false";
+            else available = "true";
+            rooms[i].available = available;
+        }
+    }
+
     console.log(rooms);
     res.send(req.query.building + " " + req.query.room);
 });
